7a333edb117cade8eec6ba379fac916f38b00a95
Jared Farago <jared.farago@gmail.com>
2025-01-15 14:49:07 -0500

    fix(utils): abstract calendar data formatting to utils folder

--- src/pages/SchedulePage.js
+++ src/pages/SchedulePage.js
@@ -11,82 +11,13 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import LockOpenIcon from "@mui/icons-material/LockOpen";
-import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
-import SportsBarIcon from "@mui/icons-material/SportsBar";
-import AccessibilityIcon from "@mui/icons-material/Accessibility";
-import SportsKabaddiIcon from "@mui/icons-material/SportsKabaddi";
-import SkateboardingIcon from "@mui/icons-material/Skateboarding";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import Breadcrumb from "../components/Breadcrumb";
 import useGoogleCalendar from "../hooks/useGoogleCalendar";
 import LoadingSpinner from "../components/LoadingSpinner";
-
-// Icon Configuration
-const iconSize = 40;
-const ICONS = {
-  clinic: <SportsEsportsIcon sx={{ fontSize: iconSize, color: "#ff9914" }} />,
-  beer: <SportsBarIcon sx={{ fontSize: iconSize, color: "#08bdbd" }} />,
-  welcome: <AccessibilityIcon sx={{ fontSize: iconSize, color: "#29bf12" }} />,
-  gateOpen: <LockOpenIcon sx={{ fontSize: iconSize, color: "#f21b3f" }} />,
-  rumble: <SportsKabaddiIcon sx={{ fontSize: iconSize, color: "#5603ad" }} />,
-  groupRide: (
-    <SkateboardingIcon sx={{ fontSize: iconSize, color: "#fface4" }} />
-  ),
-  comingSoon: <AccessTimeIcon sx={{ fontSize: iconSize, color: "gray" }} />,
-};
+import { groupEventsByDays, isToday, getDayName } from "../utils/calenderUtils";
 
 const CALENDAR_ID = process.env.REACT_APP_CALENDAR_ID;
 
-const groupEventsByDays = (events) => {
-  const grouped = {};
-  events?.items?.forEach((event) => {
-    const startDate = event.start.date || event.start.dateTime.split("T")[0];
-    if (!grouped[startDate]) grouped[startDate] = [];
-
-    const formattedTime = new Intl.DateTimeFormat("en-US", {
-      hour: "numeric",
-      minute: "numeric",
-      hour12: true,
-    }).format(new Date(event.start.dateTime || event.start.date));
-
-    grouped[startDate].push({
-      title: event.summary || "",
-      startTime: formattedTime,
-      endTime: event.end.dateTime || event.end.date,
-      description: event.description || "",
-      location: event.location || "",
-      icon: getIcon(event.summary),
-    });
-  });
-  return grouped;
-};
-
-const getIcon = (eventTitle = "") => {
-  const title = eventTitle.toLowerCase();
-  switch (true) {
-    case title.includes("clinic"):
-      return ICONS.clinic;
-    case title.includes("tour"):
-      return ICONS.welcome;
-    case title.includes("open"):
-      return ICONS.gateOpen;
-    default:
-      return ICONS.comingSoon;
-  }
-};
-
-const getDayName = (dateString) => {
-  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(
-    new Date(dateString + "T00:00:00")
-  );
-};
-
-const isToday = (dateString) => {
-  return new Date().toISOString().split("T")[0] === dateString;
-};
-
-// Main Component
 const SchedulePage = () => {
   const { data: calendarData, loading, error } = useGoogleCalendar(CALENDAR_ID);
   const groupedEvents = useMemo(() => {
