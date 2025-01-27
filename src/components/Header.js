6566ce527390f9f689a02672d06d12f03c2be85a
David Wolf <lupine86@github.com>
2025-01-26 20:25:59 -0500

    increase size of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar sx={{backgroundColor: '#24a810'}}>
-        <Typography variant="h4" component="div" sx={{padding: '20px 0', textAlign:'center', flexGrow: 1, fontWeight: 'bold'}}>
+        <Typography variant="h4" component="div" sx={{marginTop: '50px', padding: '20px 0', textAlign:'center', flexGrow: 1, fontWeight: 'bold'}}>
           Oak City Shred Fest 5
         </Typography>
       </Toolbar>
