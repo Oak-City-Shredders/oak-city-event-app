4f891a2e59157c1b342c8c4cd5dac43ae4b3aef1
David Wolf <lupine86@github.com>
2025-02-08 19:09:34 -0500

    fix under construction

--- src/pages/EmergencyServices.tsx
+++ src/pages/EmergencyServices.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { IonContent, IonPage } from "@ionic/react";
 import UnderConstruction from "../components/UnderConstruction";
+import { IonHeader, IonToolbar, IonTitle } from "@ionic/react";
 
 const EmergencyServices: React.FC = () => {
   return (
     <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Emergency Services</IonTitle>
+        </IonToolbar>
+      </IonHeader>
       <IonContent>
         <UnderConstruction />
       </IonContent>
