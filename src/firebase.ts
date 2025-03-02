919fc8a4ccdb32ad27ca31039223d156a4d74d60
David Wolf <lupine86@github.com>
2025-03-01 20:24:42 -0500

    add back analytics

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from "firebase/firestore";
 import { initializeAuth, indexedDBLocalPersistence } from 'firebase/auth';
 import { Capacitor } from '@capacitor/core';
@@ -30,6 +31,7 @@ if (Capacitor.isNativePlatform()) {
   });
 }
 
+const analytics = getAnalytics(app);
 //const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 
