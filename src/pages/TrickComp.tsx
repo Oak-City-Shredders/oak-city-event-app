47e426ac20beadbc2dd747fc50273ded7dd70c38
David Wolf <lupine86@github.com>
2025-03-05 23:54:50 -0500

    add trick comp ticket

--- src/pages/TrickComp.tsx
+++ src/pages/TrickComp.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { IonContent, IonPage } from '@ionic/react';
+import {
+  IonContent,
+  IonPage,
+  IonCard,
+  IonText,
+  IonCardContent,
+} from '@ionic/react';
 import UnderConstruction from '../components/UnderConstruction';
 import PageHeader from '../components/PageHeader';
 import NotificationToggle from '../components/NotificationToggle';
@@ -9,9 +15,33 @@ const TrickCompPage: React.FC = () => {
     <IonPage>
       <PageHeader title="Trick Comp" />
       <IonContent>
-        <img src="images/stokepark.webp" alt="Trick Comp" />
-
-        <NotificationToggle topic="trick-comp" />
+        <IonCard>
+          <img
+            src="images/stokepark.webp"
+            alt="Trick Comp"
+            style={{
+              width: '100%',
+              height: 'auto',
+              maxHeight: '300px',
+              objectFit: 'cover',
+            }}
+          />
+          <IonCardContent>
+            <IonText>
+              <a
+                href={
+                  'https://www.oakcityshredfest.com/upgrades/p/trick-competition'
+                }
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ whiteSpace: 'nowrap' }}
+              >
+                Get your trick competition ticket here
+              </a>
+            </IonText>
+            <NotificationToggle topic="trick-comp" />
+          </IonCardContent>
+        </IonCard>
         <UnderConstruction />
       </IonContent>
     </IonPage>
