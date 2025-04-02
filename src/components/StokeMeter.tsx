c56896f4a0fe7c639b0ccd2d00164616f83fc772
David Wolf <lupine86@github.com>
2025-04-01 20:54:10 -0400

    make stoke meter firebase driven

--- src/components/StokeMeter.tsx
+++ src/components/StokeMeter.tsx
@@ -10,13 +10,21 @@ import {
   IonItem,
   IonList,
   IonCard,
+  IonSkeletonText,
 } from '@ionic/react';
 import { motion } from 'framer-motion';
 import './StokeMeter.css';
 import Confetti from 'react-confetti';
 import { Haptics, ImpactStyle } from '@capacitor/haptics';
 import { checkVibrate } from '../utils/vibrate';
+import useFireStoreDB from '../hooks/useFireStoreDB';
 
+interface FireStoreStokeItem {
+  id: number;
+  text: string;
+  link?: string;
+  completed?: boolean;
+}
 interface StokeItem {
   id: number;
   text: string;
@@ -24,43 +32,46 @@ interface StokeItem {
   completed?: boolean;
 }
 
-const checklistItems: StokeItem[] = [
-  { id: 1, text: 'Installed the App', completed: true },
-  { id: 2, text: 'Cleared your calendar 4/24-4/27' },
-  { id: 3, text: 'Bought ticket', link: 'https://oakcityshredfest.com' },
-  { id: 4, text: 'Signed Waiver', link: 'https://waiver.fr/p-cSoyb' },
-  {
-    id: 5,
-    text: 'Planned lodging',
-    link: 'https://www.oakcityshredfest.com/close-to-the-farm',
-  },
-  {
-    id: 6,
-    text: 'Joined the Discord chat',
-    link: 'https://discord.gg/r9xx5V2s',
-  },
-  {
-    id: 7,
-    text: 'Ordered Shred Fest jersey',
-    link: 'https://www.oakcityshredfest.com/squirrelshop/p/2025-oak-city-race-jersey-custom-name-number',
-  },
-  { id: 8, text: 'Charged Device' },
-  { id: 9, text: 'Packed gear' },
-];
-
 export default function StokeMeter() {
+  const { data, loading, error, refetch } =
+    useFireStoreDB<FireStoreStokeItem>('StokeMeter');
+
   const [isListVisible, setIsListVisible] = useState(false);
-  const [stokeItems, setStokeItemsToStorage] = useLocalStorage<StokeItem[]>(
-    'stoke-meter-v3c',
-    checklistItems
-  );
+  const [localStokeItems, setStokeItemsToStorage] = useLocalStorage<
+    StokeItem[]
+  >('stoke-meter-v3c', [] as StokeItem[]);
+
+  const stokeItemsUpdatedWithLocal: StokeItem[] = !data
+    ? []
+    : data
+        .map(
+          (item) =>
+            ({
+              id: Number(item.id),
+              text: item.text,
+              completed: item.id === 1 ? true : false,
+              // Set the first item as completed by default
+              link: item.link,
+            } as StokeItem)
+        )
+        .map((fireBaseItem: StokeItem) => {
+          const localItem = localStokeItems.find(
+            (i) => i.id === fireBaseItem.id
+          );
+          return localItem
+            ? { ...fireBaseItem, completed: localItem.completed }
+            : fireBaseItem;
+        })
+        .sort((a, b) => a.id - b.id);
 
   const handleCheck = async (id: number) => {
     checkVibrate(
       async () => await Haptics.impact({ style: ImpactStyle.Light })
     );
-    setStokeItemsToStorage((prev) =>
-      prev.map((i) => (i.id === id ? { ...i, completed: !i.completed } : i))
+    setStokeItemsToStorage(() =>
+      stokeItemsUpdatedWithLocal.map((i) =>
+        i.id === id ? { ...i, completed: !i.completed } : i
+      )
     );
   };
 
@@ -71,7 +82,9 @@ export default function StokeMeter() {
     setIsListVisible((prev) => !prev);
   };
 
-  const completedCount = stokeItems.filter((item) => item.completed).length;
+  const completedCount = stokeItemsUpdatedWithLocal.filter(
+    (item) => item.completed
+  ).length;
 
   const subTitle = () => {
     switch (true) {
@@ -89,11 +102,28 @@ export default function StokeMeter() {
     }
   };
 
-  const progress = completedCount / stokeItems.length;
+  const progress = completedCount / stokeItemsUpdatedWithLocal.length;
 
-  return (
+  return loading ? (
     <IonCard className="stoke-meter">
-      {isListVisible && progress === 1 && <Confetti />}
+      <IonCardHeader>
+        <IonCardTitle>Stoke Meter</IonCardTitle>
+        <IonCardSubtitle>
+          <IonSkeletonText animated={true} />
+        </IonCardSubtitle>
+      </IonCardHeader>
+      <IonCardContent>
+        <div className="custom-progress-container mb-4">
+          <IonSkeletonText animated={true} />
+        </div>
+        <p className="text-lg text-center">
+          <IonSkeletonText animated={true} />
+        </p>
+      </IonCardContent>
+    </IonCard>
+  ) : (
+    <IonCard className="stoke-meter">
+      {isListVisible && !loading && progress === 1 && <Confetti />}
       <IonCardHeader onClick={onToggleView}>
         <IonCardTitle>Stoke Meter</IonCardTitle>
         <IonCardSubtitle>{subTitle()}</IonCardSubtitle>
@@ -108,8 +138,9 @@ export default function StokeMeter() {
             <IonProgressBar value={progress} className="custom-progress-bar" />
           </div>
           <p className="text-lg text-center">
-            {stokeItems.filter((i) => i.completed).length} of{' '}
-            {stokeItems.length} completed
+            {`${
+              stokeItemsUpdatedWithLocal.filter((i) => i.completed).length
+            } of ${stokeItemsUpdatedWithLocal.length} completed`}
           </p>
         </motion.div>
 
@@ -119,7 +150,7 @@ export default function StokeMeter() {
               Check off your tasks to get fully stoked for Shred Fest!
             </p>
             <IonList>
-              {stokeItems.map((item, index) => (
+              {stokeItemsUpdatedWithLocal.map((item, index) => (
                 <motion.div
                   key={index}
                   initial={{ opacity: 0, x: -10 }}
