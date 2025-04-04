f0aebb16f01595e6c2f6826b4969f65d5ac685b6
David Wolf <lupine86@github.com>
2025-04-04 15:18:55 -0400

    fix account validation flow

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   loginUser,
   registerUser,
@@ -40,6 +40,7 @@ import {
 import { useRefreshHandler } from '../hooks/useRefreshHandler';
 import { checkVibrate } from '../utils/vibrate';
 import { Haptics, ImpactStyle } from '@capacitor/haptics';
+import { App } from '@capacitor/app';
 
 const Login: React.FC = () => {
   const { user, loading, error: authError, refreshAuth } = useAuth();
@@ -48,6 +49,30 @@ const Login: React.FC = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState<string>('');
   const [isAdvancedVisible, setIsAdvancedVisible] = useState(false);
+
+  useEffect(() => {
+    // Fetch delivered notifications when app is resumed
+    let appResumedListener: { remove: () => void }; // Type for the listener handle
+    (async () => {
+      const appResumed = async () => {
+        try {
+          refreshAuth(); // Refresh auth state when app resumes
+          //const permissionStatus = await PushNotifications.checkPermissions();
+          //setNotificationPermission(permissionStatus.receive);
+        } catch (error) {
+          console.error('Error refreshing auth:', error);
+        }
+      };
+      appResumedListener = await App.addListener('resume', appResumed);
+    })();
+
+    return () => {
+      if (appResumedListener) {
+        appResumedListener.remove();
+      }
+      console.log('login cleanup - all listeners removed');
+    };
+  }, []);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
